a5967672e4be5f62149433329d3a4d62445cf15b
Ubuntu <ubuntu@ip-172-30-0-89.ec2.internal>
2015-04-06 17:58:51 +0000

    Changes to production env and unicorn for simple non ssl server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -33,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
