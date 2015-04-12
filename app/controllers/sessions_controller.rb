614a107401f2f000a18bbe725674779f33ed7822
Ted Kang <tedtkang@gmail.com>
2015-04-13 00:31:43 +0900

    added some styling to the posts admin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      #if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         ### This is all extra shit.  Just redirect to main posts page now.
-        redirect_back_or posts_path
-      else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
-        flash[:warning] = message
-        redirect_to root_url
-      end
+        redirect_back_or posts_path 
+      #else
+      #  message  = "Account not activated. "
+      #  message += "Check your email for the activation link."
+      #  flash[:warning] = message
+      #  redirect_to root_url
+      #end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
